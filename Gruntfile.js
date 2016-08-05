456138cb443bba28e4b951289dc573f508c6e4fb
pavlovicnemanja <me@pavlovicnemanja.com>
2016-08-05 11:40:31 +0200

    chore(core): Updated Gruntfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,10 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.config.init({
+        clean: {
+            options: {force: true},
+            structure: ['.tmp', 'dist']
+        },
         useminPrepare: {
             html: 'app/index.html',
             options: {dest: 'dist'}
@@ -13,23 +17,52 @@ module.exports = function (grunt) {
             html: {
                 src: 'app/index.html',
                 dest: 'dist/index.html'
+            },
+            fonts: {
+                expand: true,
+                src: 'app/styles/fonts/*',
+                dest: 'dist/assets/fonts/',
+                flatten: true
+            },
+            images: {
+                expand: true,
+                src: 'app/styles/images/*',
+                dest: 'dist/assets/images/',
+                flatten: true
             }
         },
         less: {
-            development: {
+            production: {
                 options: {
                     paths: 'app/styles',
-                    yuicompress: true
+                    rootpath: '../assets',
+                    compress: true,
+                    cleancss: true
                 },
                 files: {
-                    'app/styles/style.css': ['app/**/*.less', '!app/scripts/bower_components/**/*.less']
+                    '.tmp/style.css': ['app/**/*.less']
                 }
+            }
+        },
+        concat: {
+            options: {sourceMap: true}
+        },
+        cssmin: {
+            options: {sourceMap: true}
+        },
+        uglify: {
+            options: {
+                beautify: true,
+                sourceMap: true,
+                compress: {drop_console: true}
             }
         }
     });
 
-    grunt.registerTask('default', [
-        'copy:html',
+    grunt.registerTask('build', [
+        'clean',
+        'less',
+        'copy',
         'useminPrepare',
         'concat',
         'uglify',
